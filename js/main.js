a2c7c54582b4a5243835945e87fc1604ac033611
annaspas <annaspasenko@gmail.com>
2019-06-24 15:38:44 +0300

    services+imac

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-$(document).ready(function(){
-  $('.js_tabs-link').on('click', function(){
-      var tabNum = $(this).attr('href');
-      $(this).addClass('tab-active');
-      $(this).parent().siblings().find('.js_tabs-link').removeClass('tab-active');
-      $(tabNum).show(400);
-      $(tabNum).addClass('.tab-active');
-      $(tabNum).siblings().hide(400);
-      $(tabNum).siblings().removeClass('.tab-active');
-  });
-});
+$(document).ready(function() {
+    $('.js_tabs-link').on('click', function() {
+        var tabNum = $(this).attr('href')
+
+        console.log(tabNum)
+
+        $(this).addClass('tab-active')
+        $(this)
+            .parent()
+            .siblings()
+            .find('.js_tabs-link')
+            .removeClass('tab-active')
+        $(tabNum).show(400)
+        $(tabNum).addClass('.tab-active')
+        $(tabNum)
+            .siblings()
+            .hide(400)
+        $(tabNum)
+            .siblings()
+            .removeClass('.tab-active')
+    })
+})
